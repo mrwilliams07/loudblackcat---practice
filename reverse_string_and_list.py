92ef0e21ee34ff4195d8b7636cfffb558d983247
mrwilliams07 <esowilliams07@gmail.com>
2024-05-10 12:13:27 -0400

    Create reverse_string_and_list.py

---
+++ reverse_string_and_list.py
@@ -0,0 +1,9 @@
+# Reverse a string
+string = "hello"
+reversed_string = string[::-1]
+print(reversed_string)  # Output: "olleh"
+
+# Reverse a list
+my_list = [1, 2, 3, 4, 5]
+reversed_list = my_list[::-1]
+print(reversed_list)  # Output: [5, 4, 3, 2, 1]
